931d6d75e89ff4e85b3ea716dd6414c673ce0244
Breedwizzy <breedwizzy@yahoo.com>
2025-05-14 16:03:51 +0100

    insssp

--- alembic/versions/c94450fb918c_add_profile_and_nin_photo_fields_to_.py
+++ alembic/versions/c94450fb918c_add_profile_and_nin_photo_fields_to_.py
@@ -8,16 +8,28 @@ Create Date: 2024-11-11 23:05:15.713334
 from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy import inspect
 
 # revision identifiers
 revision: str = 'c94450fb918c'
-down_revision: Union[str, None] = '57a0ac249180'
+down_revision: str = '57a0ac249180'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
-    op.add_column('riders', sa.Column('rider_photo', sa.String(), nullable=True))
-    op.add_column('riders', sa.Column('nin_photo', sa.String(), nullable=True))
+    conn = op.get_bind()
+    inspector = inspect(conn)
+
+    # Get a list of columns in the 'riders' table
+    columns = [col['name'] for col in inspector.get_columns('riders')]
+
+    # Check if 'rider_photo' exists before adding it
+    if 'rider_photo' not in columns:
+        op.add_column('riders', sa.Column('rider_photo', sa.String(), nullable=True))
+
+    # Check if 'nin_photo' exists before adding it
+    if 'nin_photo' not in columns:
+        op.add_column('riders', sa.Column('nin_photo', sa.String(), nullable=True))
 
 def downgrade() -> None:
     op.drop_column('riders', 'nin_photo')
