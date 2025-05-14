59f4436c563384aea76f74e8a903ead374f1679c
Breedwizzy <breedwizzy@yahoo.com>
2025-05-14 15:48:06 +0100

    mmiii

--- alembic/versions/c94450fb918c_add_profile_and_nin_photo_fields_to_.py
+++ alembic/versions/c94450fb918c_add_profile_and_nin_photo_fields_to_.py
@@ -6,24 +6,19 @@ Create Date: 2024-11-11 23:05:15.713334
 
 """
 from typing import Sequence, Union
-
 from alembic import op
 import sqlalchemy as sa
 
-# revision identifiers, used by Alembic.
+# revision identifiers
 revision: str = 'c94450fb918c'
 down_revision: Union[str, None] = '57a0ac249180'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
-
 def upgrade() -> None:
-    # Add the columns with correct types (if they don't exist already)
     op.add_column('riders', sa.Column('rider_photo', sa.String(), nullable=True))
     op.add_column('riders', sa.Column('nin_photo', sa.String(), nullable=True))
 
-
 def downgrade() -> None:
-    # Drop the columns if rolling back
     op.drop_column('riders', 'nin_photo')
     op.drop_column('riders', 'rider_photo')
